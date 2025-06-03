533a725cccb65f2c944da41d7524bdba5b56a4bd
Oscar Lista Rivera <olista94@gmail.com>
2025-06-03 20:18:03 +0200

    feature
    agregada llamada del controlador en la ruta de recurso

--- backend/routes/resourceRoutes.js
+++ backend/routes/resourceRoutes.js
@@ -2,13 +2,13 @@ const express = require('express');
 const router = express.Router();
 const ResourceController = require('../controllers/ResourceController');
 const auth = require('../middlewares/auth');
+const isAdmin = require('../middlewares/isAdmin');
 const upload = require("../middlewares/multer");
-
 
 router.get('/', ResourceController.getAll);
 router.get('/:id', ResourceController.getById);
 router.post('/', auth, upload.single("file"), ResourceController.uploadWithFile);
 router.put('/:id', auth, ResourceController.update);
 router.delete('/:id', auth, ResourceController.remove);
-
+router.get('/admin/resources', auth, isAdmin, ResourceController.getAll);
 module.exports = router;
