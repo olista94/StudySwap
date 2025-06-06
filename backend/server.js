1523ddc448b6d049bfbe376a593d35496d8b66e4
olista94 <olista94@gmail.com>
2025-06-06 14:25:57 +0200

    bug
    solucionado error en la actualización de los datos

--- backend/server.js
+++ backend/server.js
@@ -22,8 +22,6 @@ app.use(cors({
 
 app.use(express.json());
 
-// app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
-
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Rutas API
